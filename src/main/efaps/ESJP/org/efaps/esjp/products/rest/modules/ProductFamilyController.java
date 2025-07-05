8104147c9ff79be0ca0144ac483ce9fbfaaa0fae
Jan Moxter <jan.moxter@moxter.net>
2025-07-05 18:46:56 -0500

    ProductFamily UIModule

---
+++ src/main/efaps/ESJP/org/efaps/esjp/products/rest/modules/ProductFamilyController.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright © 2003 - 2024 The eFaps Team (-)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.efaps.esjp.products.rest.modules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Instance;
+import org.efaps.eql.EQL;
+import org.efaps.esjp.ci.CIProducts;
+import org.efaps.util.EFapsException;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@EFapsUUID("69b79f5b-b571-400f-8a1f-68c8c2e9c9f7")
+@EFapsApplication("eFapsApp-Products")
+@Path("/ui/modules/product-family")
+public class ProductFamilyController
+{
+
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response getReport()
+        throws EFapsException
+    {
+        return Response.ok(loadFamilyTree())
+                        .build();
+    }
+
+    public List<ProductFamilyDto> loadFamilyTree()
+        throws EFapsException
+    {
+        final var dtos = new ArrayList<ProductFamilyDto>();
+        final var eval = EQL.builder().print().query(CIProducts.ProductFamilyRoot)
+                        .select()
+                        .attribute(CIProducts.ProductFamilyRoot.Name, CIProducts.ProductFamilyRoot.CodePart)
+                        .evaluate();
+        while (eval.next()) {
+            dtos.add(ProductFamilyDto.builder()
+                            .withOid(eval.inst().getOid())
+                            .withLabel(eval.get(CIProducts.ProductFamilyRoot.Name))
+                            .withCodePart(eval.get(CIProducts.ProductFamilyRoot.CodePart))
+                            .withChildren(loadChildren(eval.inst()))
+                            .build());
+        }
+        return dtos;
+    }
+
+    public List<ProductFamilyDto> loadChildren(final Instance parentInst)
+        throws EFapsException
+    {
+        final var dtos = new ArrayList<ProductFamilyDto>();
+        final var eval = EQL.builder().print().query(CIProducts.ProductFamilyStandart)
+                        .where()
+                        .attribute(CIProducts.ProductFamilyStandart.ParentLink).eq(parentInst)
+                        .select()
+                        .attribute(CIProducts.ProductFamilyStandart.Name, CIProducts.ProductFamilyStandart.CodePart)
+                        .evaluate();
+        while (eval.next()) {
+            dtos.add(ProductFamilyDto.builder()
+                            .withOid(eval.inst().getOid())
+                            .withLabel(eval.get(CIProducts.ProductFamilyStandart.Name))
+                            .withCodePart(eval.get(CIProducts.ProductFamilyStandart.CodePart))
+                            .withChildren(loadChildren(eval.inst()))
+                            .withParentOid(parentInst.getOid())
+                            .build());
+        }
+        return dtos;
+    }
+}
---
+++ src/main/efaps/ESJP/org/efaps/esjp/products/rest/modules/ProductFamilyDto.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright © 2003 - 2024 The eFaps Team (-)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.efaps.esjp.products.rest.modules;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@EFapsUUID("89f7c4e9-1849-4a90-8e2c-b38fa2f323c5")
+@EFapsApplication("eFapsApp-Products")
+@JsonDeserialize(builder = ProductFamilyDto.Builder.class)
+public class ProductFamilyDto
+{
+
+    private final String oid;
+    private final String label;
+    private final String codePart;
+    private final String parentOid;
+    private final List<ProductFamilyDto> children;
+
+    private ProductFamilyDto(Builder builder)
+    {
+        this.oid = builder.oid;
+        this.label = builder.label;
+        this.codePart = builder.codePart;
+        this.parentOid = builder.parentOid;
+        this.children = builder.children;
+    }
+
+    public String getOid()
+    {
+        return oid;
+    }
+
+    public String getLabel()
+    {
+        return label;
+    }
+
+    public String getCodePart()
+    {
+        return codePart;
+    }
+
+    public String getParentOid()
+    {
+        return parentOid;
+    }
+
+    public List<ProductFamilyDto> getChildren()
+    {
+        return children;
+    }
+
+    public static Builder builder()
+    {
+        return new Builder();
+    }
+
+    public static final class Builder
+    {
+
+        private String oid;
+        private String label;
+        private String codePart;
+        private String parentOid;
+        private List<ProductFamilyDto> children = Collections.emptyList();
+
+        private Builder()
+        {
+        }
+
+        public Builder withOid(String oid)
+        {
+            this.oid = oid;
+            return this;
+        }
+
+        public Builder withLabel(String label)
+        {
+            this.label = label;
+            return this;
+        }
+
+        public Builder withCodePart(String codePart)
+        {
+            this.codePart = codePart;
+            return this;
+        }
+
+        public Builder withParentOid(String parentOid)
+        {
+            this.parentOid = parentOid;
+            return this;
+        }
+
+        public Builder withChildren(List<ProductFamilyDto> children)
+        {
+            this.children = children;
+            return this;
+        }
+
+        public ProductFamilyDto build()
+        {
+            return new ProductFamilyDto(this);
+        }
+    }
+
+}
